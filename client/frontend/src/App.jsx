ff7ee731610c45c89cae9099d39605579ec75c2e
Amit Nayak <amitchiku2006@gmail.com>
2026-01-30 22:14:00 +0530

    react and tailwind was successfully confuired

---
+++ client/frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1 class="text-3xl font-bold underline text-sky-500 text-center mt-10 tracking-wide hover:text-sky-700 transition duration-300">
+  Hello alok welcome to gyan's project
+</h1>
+
+    </>
+  )
+}
+
+export default App;
